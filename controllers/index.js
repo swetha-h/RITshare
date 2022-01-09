37c455d65e6b7b823489b68e7ae25c3005ad3917
swetha-h <swetha-h@users.noreply.github.com>
2022-01-09 17:11:49 +0000

    Prettified Code!

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var path = require("path");
 const eventModel = require("../models/events");
 
 module.exports = function (req, res, next) {
-  eventModel.find({date: { $gte: Date.now() }}, function (err, upcoming) {
+  eventModel.find({ date: { $gte: Date.now() } }, function (err, upcoming) {
     if (err) throw error;
     else {
       res.render("index", { event: upcoming });
