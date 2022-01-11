f0763e6e21734b4f959e6f60d1827150a88d2c1b
saitanmayee01 <sai.penupati@gmail.com>
2022-01-11 20:05:51 +0530

    logged in page and routing to it

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,21 @@
 var path = require("path");
 const eventModel = require("../models/events");
+const clubModel = require("../models/club");
 
-module.exports = function (req, res, next) {
+module.exports = {
+  dummy: function(req, res,next) {
+    res.render("index")
+  },
+  
+  loggedin:function (req, res, next) {
+  let clubs = clubModel.find({}).exec();
   eventModel.find({ date: { $gte: Date.now() } }, function (err, upcoming) {
     if (err) throw error;
     else {
-      res.render("index", { event: upcoming });
+      res.render("loggedin", { event: upcoming, club:clubs });
     }
   });
-};
+
+  
+},
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var indexController = require("../controllers/index");
 
 /* GET home page. */
-router.get("/", indexController);
+router.get("/", indexController.dummy);
+router.get("/loggedin", indexController.loggedin);
 
 module.exports = router;
