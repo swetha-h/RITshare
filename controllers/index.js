b8d01ec4659ff24a2f1b60e6b3bf6e8a8283bdc3
Swetha H <swethaswethu0002@gmail.com>
2022-01-11 23:12:13 +0530

    added add event button

--- controllers/index.js
+++ controllers/index.js
@@ -7,12 +7,15 @@ module.exports = {
     res.render("index");
   },
 
-  loggedin: function (req, res, next) {
-    let clubs = clubModel.find({}).exec();
+  loggedin: async function (req, res, next) {
+    let clubsLead = await clubModel.find({}, { leadUSN: 1 }).exec();
     eventModel.find({ date: { $gte: Date.now() } }, function (err, upcoming) {
       if (err) throw error;
       else {
-        res.render("loggedin", { event: upcoming, club: clubs });
+        res.render("loggedin", {
+          event: upcoming,
+          isTeamLead: clubsLead.find(record => record.leadUSN == req.query.usn)
+        });
       }
     });
   },
